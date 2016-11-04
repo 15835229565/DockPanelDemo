84a77bfa9f945eaa020462f282ca757916fd1baf
MichealMacVitrual <gxz04220427@163.com>
2016-11-05 00:51:35 +0800

    完成项目

--- DockPanelDemo/MainForm.cs
+++ DockPanelDemo/MainForm.cs
@@ -7,14 +7,41 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using WeifenLuo.WinFormsUI.Docking;
 
 namespace DockPanelDemo
 {
     public partial class MainForm : Form
     {
+        private ToolFormA toolFormA;
+        private ToolFormB toolFormB;
+        private ToolFormC toolFormC;
+        private WorkFormA workFormA;
+        private WorkFormB workFormB;
+        private WorkFormC workFormC;
         public MainForm()
         {
             InitializeComponent();
+            this.Load += MainForm_Load;
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            VS2013BlueTheme theme = new VS2013BlueTheme();
+            this.MainPanel.Theme = theme;
+            this.toolFormA=new ToolFormA();
+            this.toolFormB=new ToolFormB();
+            this.toolFormC=new ToolFormC();
+            this.workFormA=new WorkFormA();
+            this.workFormB=new WorkFormB();
+            this.workFormC=new WorkFormC();
+            toolFormA.Show(this.MainPanel,DockState.DockLeft);
+            toolFormB.Show(this.MainPanel, DockState.DockLeft);
+            toolFormC.Show(this.MainPanel, DockState.DockLeft);
+            workFormA.Show(this.MainPanel,DockState.Document);
+            workFormB.Show(this.MainPanel, DockState.Document);
+            workFormC.Show(this.MainPanel, DockState.Document);
+            
         }
     }
 }
---
+++ DockPanelDemo/WorkFormB.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace DockPanelDemo
+{
+    partial class WorkFormB
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(166, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "这是工作窗体B";
+            // 
+            // WorkFormB
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Teal;
+            this.ClientSize = new System.Drawing.Size(1064, 741);
+            this.Controls.Add(this.label1);
+            this.Name = "WorkFormB";
+            this.Text = "WorkFormB";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+    }
+}
